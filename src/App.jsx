69491d1149a6aa274a6111f48cc14ebaa4b48755
Emma Adey <eadey@usf.edu>
2025-04-06 16:52:48 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greetings from "./components/Greetings";
+
+function App() {
+    return (
+      <div>
+        <h1>Welcome to My React App!</h1>
+        <Greetings name="Wayne Gretzky" />
+      </div>
+    );
+}
+export default App;
